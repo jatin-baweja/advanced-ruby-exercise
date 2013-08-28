11961e9207f92bc41bc4cae27ea907f33dccf5d6
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-28 13:32:52 +0530

    solution to Interactive program Exercise

--- exercise2/bin/main.rb
+++ exercise2/bin/main.rb
@@ -6,7 +6,7 @@ puts "Enter a ruby program (Enter blank line to evaluate, enter 'q' to quit) :"
 loop do
   input_statement = gets
   break if input_statement.chomp == ""
-  exit if input_statement.chomp == /^q$/i
+  exit if input_statement.chomp =~ /^q$/i
   statements = statements.concat(input_statement)
 end
 execute_program(statements)
